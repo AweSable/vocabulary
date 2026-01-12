e1b98e83cdbd0e694fb75c403c2d08dc918b1883
AweSable <DA.Poryvaev@gmail.com>
2026-01-12 20:39:48 +0400

    feat: add next.js framework

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+        permanent: true,
+      },
+    ]
+  },
+}
